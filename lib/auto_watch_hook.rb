c2e3d0b88c97c05eeb003683562a9c8566e0ee47
Thorin <thorin.iv@gmail.com>
2012-10-28 22:35:29 +0100

    Enabled auto watch for bulk edit and new issues

--- lib/auto_watch_hook.rb
+++ lib/auto_watch_hook.rb
@@ -1,12 +1,35 @@
 # Hooks to attach to the Redmine Issues.
 class AutoWatchHook < Redmine::Hook::Listener
-  def controller_issues_edit_before_save(context = { })
-    @issue = context[:issue]
-    unless @issue.watched_by?(User.current) || @issue.author == User.current
-      @issue.add_watcher(User.current)
+  def controller_issues_edit_before_save(context = {})
+    issue = context[:issue]
+
+    add_current_user(issue)
+    add_assignee(issue)
+  end
+
+  def controller_issues_new_before_save(context = {})
+    add_assignee(context[:issue])
+  end
+
+
+  def controller_issues_bulk_edit_before_save(context = {})
+    issue = context[:issue]
+
+    add_current_user(issue)
+    add_assignee(issue)
+  end
+
+  private
+  def add_current_user(issue)
+    unless issue.watched_by?(User.current) # || issue.author == User.current
+      issue.add_watcher(User.current)
     end
-    unless @issue.assigned_to == nil || (not @issue.assigned_to === User) || @issue.watched_by?(@issue.assigned_to) || @issue.author == @issue.assigned_to
-      @issue.add_watcher(@issue.assigned_to)
+  end
+    
+  def add_assignee(issue)
+    assignee = issue.assigned_to
+    unless assignee.nil? || !assignee.is_a?(User) || issue.watched_by?(assignee) # || issue.author == issue.assigned_to
+      issue.add_watcher(assignee)
     end
   end
 end
